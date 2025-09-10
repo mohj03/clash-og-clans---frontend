e0703e4c6f3024ecf177f738bff298c3d32ea3a4
Magnus <magnus.hajohansen@gmail.com>
2025-09-10 23:53:52 +0200

    Fikset logo og live-indikatorer

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/clash-og-clans---frontend/',   // Adjust base path for GitHub Pages
+  base: './',  // bruk relative paths
   plugins: [react()],
 })
