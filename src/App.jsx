0b73c5d54c9d6267798082a3b2fb1a14a33f806c
Magnus <magnus.hajohansen@gmail.com>
2025-09-10 22:57:59 +0200

    Initial commit: frontend prosjekt

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Clash of Clans Frontend 🚀</h1>
+      <p>Her skal vi vise data fra backend etter hvert.</p>
+    </div>
+  )
+}
+
+export default App
